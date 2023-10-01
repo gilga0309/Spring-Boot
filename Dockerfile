c7cb4e8e95fb5eaa7ed56b47733bb90f0c9d3a27
gilga0309 <gilga0309@gmail.com>
2023-10-01 22:26:03 +0900

    File edit deploy, dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,14 @@
-FROM openjdk:17.0.8-jdk-slim
-WORKDIR /app
-ADD . /app
+# docker 생성 이미지 openjdk 17 버전
+FROM openjdk:17-jdk
+
+# 이미지 지정 port 8080
+EXPOSE 8080
+
+# JAR_FILE 변수 정의 -> 기본적으로 jar file이 2개이기 때문에 이름을 특정한다.
+ARG JAR_FILE=target/*.jar
+
+# JAR 파일 메인 디렉토리에 복사
+COPY ${JAR_FILE} app.jar
+
+# 시스템 진입점 정의
+ENTRYPOINT ["java","-jar","/app.jar"]
--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-APP_NAME=<product>
-REPOSITORY=/home/ubuntu/
+APP_NAME=<product-docker> #도커 이미지 및 컨테이너 이름
+REPOSITORY=/home/ubuntu/ #빌드 파일의 경로
 
 echo "> Check the currently running container"
 CONTAINER_ID=$(docker ps -aqf "name=$APP_NAME")
 
-if [ -z "$CONTAINER_ID" ];
+if [ -z "$CONTAINER_ID" ]; #현재 같은 이름으로 실행중인(사전에 배포된) 컨테이너가 있다면 중지, 컨테이너 삭제
 then
   echo "> No such container is running."
 else
@@ -15,11 +15,11 @@ else
   docker rm "$CONTAINER_ID"
 fi
 
-echo "> Remove previous Docker image"
+echo "> Remove previous Docker image" #같은 이름으로 빌드된 도커 이미지(사전에 빌드된)를 삭제
 docker rmi "$APP_NAME"
 
-echo "> Build Docker image"
+echo "> Build Docker image" #도커 이미지를 빌드
 docker build -t "$APP_NAME" "$REPOSITORY"
 
-echo "> Run the Docker container"
+echo "> Run the Docker container" #도커 컨테이너 생성
 docker run -d -p 3000:8080 --name "$APP_NAME" "$APP_NAME"
